42e88caada95f401a73b317ff96e2cc0c9f16efd
Kaati <kaatipuola@gmail.com>
2024-04-21 21:22:35 +0300

    update gendiff/scripts/gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+from gendiff.gendiff import generate_diff
 
 
 def main():
@@ -12,6 +13,10 @@ def main():
     parser.add_argument('-f', '--format',
                         help='set format of output')
     args = parser.parse_args()
+    first_file = args.first_file
+    second_file = args.second_file
+    print(generate_diff(first_file, second_file))
+
 
 if __name__ == '__main__':
     main()
