53636c2ffa2d6205e330e5de8797ce4c0fc3780b
Kaati <kaatipuola@gmail.com>
2024-04-26 16:33:15 +0300

    Add gendiff/parser.py

---
+++ gendiff/parser.py
@@ -0,0 +1,16 @@
+import os
+import json
+import yaml
+
+
+def parse_data(file, extension):
+    if extension == 'json':
+        return json.load(file)
+    elif extension in ['yaml', 'yml']:
+        return yaml.safe_load(file)
+
+
+def get_extension(file_path):
+    _, file_extension = os.path.splitext(file_path)
+    with open(file_path, 'r') as file:
+        return parse_data(file, file_extension[1:])
