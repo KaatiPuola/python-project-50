771666e08c99d9738ed8d389b3a77f6ab66b778e
Kaati <kaatipuola@gmail.com>
2024-05-03 23:20:40 +0300

    Update gendiff/scripts/gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -10,8 +10,12 @@ def main():
     )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f', '--format',
-                        help='set format of output')
+    parser.add_argument(
+        '-f', '--format',
+        help='set format of output',
+        choices=['stylish'],
+        default='stylish'
+    )
     args = parser.parse_args()
     first_file = args.first_file
     second_file = args.second_file
