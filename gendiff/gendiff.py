7836f340efb60e163dd6270c22ffcc8154f74cf8
Kaati <kaatipuola@gmail.com>
2024-05-04 14:40:16 +0300

    Update gendiff/gendiff.py

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,10 +1,10 @@
 from gendiff.parser import get_extension
 from gendiff.diff import build_diff
-from gendiff.stylish import build_stylish
+from gendiff.formatters.formatter import get_format
 
 
 def generate_diff(file_path1, file_path2, formatter='stylish'):
     file1 = get_extension(file_path1)
     file2 = get_extension(file_path2)
     diff = build_diff(file1, file2)
-    return build_stylish(diff)
+    return get_format(diff, formatter)
