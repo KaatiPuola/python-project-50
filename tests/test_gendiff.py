8233d7e989ec02b0a8cec3b46a35940a82181392
Kaati <kaatipuola@gmail.com>
2024-05-04 14:36:30 +0300

    Update tests/test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -11,17 +11,19 @@ def get_path(file_name):
     return os.path.join(CONST_PATH, file_name)
 
 
-@pytest.mark.parametrize('name_file1, name_file2, expected_result',[
-        ('file1.json', 'file2.json', 'result_json.txt'),
-        ('file1.yml', 'file2.yml', 'result_json.txt'),
-        ('nested_file1.json', 'nested_file2.json', 'result_nested.txt'),
-        ('nested_file1.yaml', 'nested_file2.yaml', 'result_nested.txt')
+@pytest.mark.parametrize('name_file1, name_file2, formatter, expected_result',[
+        ('file1.json', 'file2.json', 'stylish', 'result_json.txt'),
+        ('file1.yml', 'file2.yml', 'stylish', 'result_json.txt'),
+        ('nested_file1.json', 'nested_file2.json', 'stylish', 'result_nested.txt'),
+        ('nested_file1.yaml', 'nested_file2.yaml', 'stylish', 'result_nested.txt'),
+        ('nested_file1.json', 'nested_file2.json', 'plain', 'result_plain.txt'),
+        ('nested_file1.yaml', 'nested_file2.yaml', 'plain', 'result_plain.txt')
 ])
 
 
-def test_generate_diff(name_file1, name_file2, expected_result):
+def test_generate_diff(name_file1, name_file2, formatter, expected_result):
     with open(get_path(expected_result), 'r') as file:
-        data_result = file.read()
+        data_result = file.read().strip('\n')
     test_file1 = get_path(name_file1)
     test_file2 = get_path(name_file2)
-    assert generate_diff(test_file1, test_file2) == data_result
+    assert generate_diff(test_file1, test_file2, formatter) == data_result
