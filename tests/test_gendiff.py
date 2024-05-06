9d5aabf72d573d18d2ebe8b7894aca171936ef7b
Kaati <kaatipuola@gmail.com>
2024-05-06 12:35:22 +0300

    Update tests/test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -12,17 +12,15 @@ def get_path(file_name):
 
 
 @pytest.mark.parametrize('name_file1, name_file2, formatter, expected_result', [
-       ('file1.json', 'file2.json', 'stylish', 'result_json.txt'),
-       ('file1.yml', 'file2.yml', 'stylish', 'result_json.txt'),
-       ('nested_file1.json', 'nested_file2.json', 'stylish', 'result_nested.txt'),
-       ('nested_file1.yaml', 'nested_file2.yaml', 'stylish', 'result_nested.txt'),
-       ('nested_file1.json', 'nested_file2.json', 'plain', 'result_plain.txt'),
-       ('nested_file1.yaml', 'nested_file2.yaml', 'plain', 'result_plain.txt'),
-       ('nested_file1.yaml', 'nested_file2.yaml', 'json', 'result_format_json.txt'),
-       ('nested_file1.json', 'nested_file2.json', 'json', 'result_format_json.txt')
+    ('file1.json', 'file2.json', 'stylish', 'result_json.txt'),
+    ('file1.yml', 'file2.yml', 'stylish', 'result_json.txt'),
+    ('nested_file1.json', 'nested_file2.json', 'stylish', 'result_nested.txt'),
+    ('nested_file1.yaml', 'nested_file2.yaml', 'stylish', 'result_nested.txt'),
+    ('nested_file1.json', 'nested_file2.json', 'plain', 'result_plain.txt'),
+    ('nested_file1.yaml', 'nested_file2.yaml', 'plain', 'result_plain.txt'),
+    ('nested_file1.yaml', 'nested_file2.yaml', 'json', 'result_format_json.txt'),
+    ('nested_file1.json', 'nested_file2.json', 'json', 'result_format_json.txt')
 ])
-
-
 def test_generate_diff(name_file1, name_file2, formatter, expected_result):
     with open(get_path(expected_result), 'r') as file:
         data_result = file.read().strip('\n')
