696601427ec29058b8dda46e632950390142b8b0
Kaati <kaatipuola@gmail.com>
2024-05-09 19:46:00 +0300

    Update gendiff/formatters/formatter.py

--- gendiff/formatters/formatter.py
+++ gendiff/formatters/formatter.py
@@ -10,3 +10,5 @@ def get_format(diff, formatter):
         return build_plain(diff)
     elif formatter == 'json':
         return build_json(diff)
+    else:
+        raise ValueError('Incorrect format!')
