7d1956dd4a046c49f1f190136e6278e4720bc271
Kaati <kaatipuola@gmail.com>
2024-05-14 20:16:08 +0300

    Update tests/test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -4,11 +4,11 @@ import os
 from gendiff.gendiff import generate_diff
 
 
-CONST_PATH = './tests/fixtures/'
+FIXTURE_PATH = './tests/fixtures/'
 
 
 def get_path(file_name):
-    return os.path.join(CONST_PATH, file_name)
+    return os.path.join(FIXTURE_PATH, file_name)
 
 
 @pytest.mark.parametrize('name_file1, name_file2, formatter, expected_result', [
@@ -23,7 +23,7 @@ def get_path(file_name):
 ])
 def test_generate_diff(name_file1, name_file2, formatter, expected_result):
     with open(get_path(expected_result), 'r') as file:
-        data_result = file.read().strip('\n')
+        data_result = file.read()
         test_file1 = get_path(name_file1)
         test_file2 = get_path(name_file2)
         assert generate_diff(test_file1, test_file2, formatter) == data_result
